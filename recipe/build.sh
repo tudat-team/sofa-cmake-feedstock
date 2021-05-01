56d47686589eb0fdb962a45e671d692d0ed15d4d
ggarrett13 <g.h.garrett13@gmail.com>
2021-05-01 22:30:41 +0200

    Disabled ctest for osx_arm6

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,15 +5,17 @@ mkdir build
 cd build
 
 cmake \
-    -DCMAKE_CXX_STANDARD=14 \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-    -DCMAKE_PREFIX_PATH="$PREFIX" \
-    -DSOFA_BUILD_STATIC_LIBRARY=1 \
-    ..
+  -DCMAKE_CXX_STANDARD=14 \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  -DCMAKE_PREFIX_PATH="$PREFIX" \
+  -DSOFA_BUILD_STATIC_LIBRARY=1 \
+  ..
 
 make
 
-ctest -T Test
+if [ "$HOST_PLATFORM" != "osx-arm64" ]; then
+  ctest -T Test
+fi
 
 make install
